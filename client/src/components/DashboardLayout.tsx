00307a37025734f4af24d31c917bedc7438ac4df
jcheng510 <jade@superhumn.co>
2026-03-16 23:13:40 +0700

    Add missing ClipboardCheck import from lucide-react
    Signed-off-by: jcheng510 <jade@superhumn.co>

--- client/src/components/DashboardLayout.tsx
+++ client/src/components/DashboardLayout.tsx
@@ -3,7 +3,7 @@ import { NotificationCenter } from "@/components/NotificationCenter";
 import { AutonomousAgentBar } from "@/components/AutonomousAgentBar";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
-import {
+import 
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
@@ -59,7 +59,7 @@ import {
   Send,
   MapPin,
   ArrowRightLeft,
-  ClipboardList,
+  ClipboardCheck,  ClipboardList,
   PackageCheck,
   Brain,
   Plug,
